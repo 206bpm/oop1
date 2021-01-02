c9f4e352fa78b8548e8db1edde90510f29d5f519
206bpm <75217433+206bpm@users.noreply.github.com>
2021-01-02 21:28:44 +0200

    Add files via upload

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value: ");
+            double m;
+            double x = Convert.ToDouble(Console.ReadLine());
+            m = Math.Pow(x, 2) + 2 * x + Math.Atan(x / 2);
+            Console.WriteLine($"m = {m}");
+        }
+    }
+}
